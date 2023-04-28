8cf2ae07742121e2c0c99ec118603a9ab5ecc3d1
Ericjean17 <ericjean050@gmail.com>
2023-04-28 10:15:51 -0400

    Added some print statements

--- AlexAndEricRefactoringAssignment.java
+++ AlexAndEricRefactoringAssignment.java
@@ -2,5 +2,8 @@ class Main{
     public static void main(String[] args){
         System.out.print("Hello world");
         System.out.print("Bye world");
+        System.out.print("Hi");
+        System.out.print("Yo");
+        System.out.print("sdhfiushfioewhnfi");
     }
 }
