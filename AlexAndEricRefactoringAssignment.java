39a19caf53dc75dd8e8d93ee843fdd68e9d5f6bd
Alex <alexmamoukarys@outlook.com>
2023-04-28 10:22:40 -0400

    Alex's Commit Test

--- AlexAndEricRefactoringAssignment.java
+++ AlexAndEricRefactoringAssignment.java
@@ -2,5 +2,7 @@ class Main{
     public static void main(String[] args){
         System.out.print("Hello world");
         System.out.print("Bye world");
+
+        System.out.println("Alex's test 1");
     }
 }
