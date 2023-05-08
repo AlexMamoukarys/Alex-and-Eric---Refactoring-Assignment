29a6c11a4395f452ec2b457b9ca06eec36e1ae3e
Alpacaz4Life <124852072+Alpacaz4Life@users.noreply.github.com>
2023-05-08 17:26:13 -0400

    Made comments on FileData

--- FileData.java
+++ FileData.java
@@ -4,36 +4,40 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-// The parent class
-// class FileData {
-abstract class FileData {
-
-    //public String[] contents; // Contents are the (I assume) haikus? // NO, CONTENTS ARE THE LINES OF THE HAIKU
-    //public static String name; All files have a name // NO NEED BECAUSE NAME CAN JUST BE AQUIRED USING getName()
-
-    // Maybe this is the getContents() method from the UML diagram? // SURE
-    
-    // Reads a file and stores the contents into a Haiku object(?) of ONE haiku
+/**
+ * The parent class of LibraryData and SubmissionData
+ * 
+ * This class is abstract because its methods are inherited; there is no need to instantiate a FileData object
+ */
+abstract class FileData {    
+    /**
+     * Reads a file and stores its contents into a Haiku Object
+     * 
+     * This method was intended to work for both LibraryData and SubmissionData
+     * However, we could not figure out how to make it work for both
+     * So, we made another addContents() in SubmissionData
+     * 
+     * @param file the file to be read
+     * @param haiku the Haiku Object to write file contents to 
+     * @throws IOException
+     */
     public void addContents(File file, Haiku haiku) throws IOException{
+        // Initializes BufferedReader
         BufferedReader inputStream = null;
         String line;
+        
+        // Used to pass each line that BufferedReader to a new cell in the Haiku's contents attribute 
         int i = 0;
         
         try{
             inputStream = new BufferedReader(new FileReader(file));
             while((line = inputStream.readLine()) != null){
         
-                // Skips all the empty lines inside the file (So the spaces after each haiku)
+                // Skips all the empty lines inside the file (the spaces after each haiku)
                 if (!line.equals("") || line.equals(null)){
                     
                     // Appends the read line into an array
                     haiku.contents[i] = line;
-                }
-                
-                // TEMPORARY FIX (DOESN'T EVEN FULLY FIX THE PROBLEM) (SEE COMMENT ON PlagiarismChecker.java)
-                // When BuffedReader reads 3 lines, the while loop will break (so it reads one haiku)
-                if(i == 3){
-                    break;
                 }
 
                 i++;
@@ -51,7 +55,7 @@ abstract class FileData {
         }
     }
 
-    // Appends a Haiku object into the ArrayList of Haiku objects
+    // Appends a Haiku object into the ArrayList of Haiku objects (used for library and submissio)
     public static void addToArrayList(ArrayList<Haiku> storageArrayList, Haiku haiku){
         storageArrayList.add(haiku);
     }
