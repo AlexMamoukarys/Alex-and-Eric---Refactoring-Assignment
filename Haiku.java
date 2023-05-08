a6c5cfd180ee516945c0b2d1bfac4f9cae940863
Alpacaz4Life <124852072+Alpacaz4Life@users.noreply.github.com>
2023-05-08 17:30:04 -0400

    Made comments to Haiku.java

--- Haiku.java
+++ Haiku.java
@@ -1,13 +1,22 @@
 class Haiku {
+    // The name attribute is private and can only be accessed using the getter and setter methods below
     private String name;
     protected String[] contents = new String[3];
 
-    // Setter method - Sets a file name equal to a String variable to get the name of a file
+    /**
+     * Setter method - Accepts a file name (in the form of a String variable) to set the name of the file in the Haiku Object
+     * 
+     * @param name the name of the file that the Haiku is from
+     */
     public void setFileName(String name){
         this.name = name;
     }
 
-    // Getter method  - Returns the file name
+    /**
+     * Getter method  - Returns the file name
+     * 
+     * @return the name of the file that the Haiku is from
+     */
     public String getFileName(){
         return this.name;
     }
