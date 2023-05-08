2b1d05ef2fc58ab17783d66eaa85a62f4e9958b3
Alpacaz4Life <124852072+Alpacaz4Life@users.noreply.github.com>
2023-05-08 17:17:28 -0400

    Added comments to main and PlagiarismChecker

--- AlexAndEricRefactoringAssignment.java
+++ AlexAndEricRefactoringAssignment.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 class AlexAndEricRefactoringAssignment{
     public static void main(String[] args) throws IOException{
+        // Creates the Plagiarism Checker Object to act as a facade to run the important methods 
         PlagiarismChecker plagiarismCheckerObject = new PlagiarismChecker();
         plagiarismCheckerObject.checkPlagiarism();
     }
